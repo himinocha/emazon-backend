a180f3076e97434688ec647d20c2e9b4fe1f05d5
Mino Cha (Isaac) <mino.cha@emory.edu>
2022-02-20 23:24:59 -0500

    initiate heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8888;
+let port = process.env.PORT || 8888;
 const data = require('./data.js')
 
 app.get("/", (req,res) => res.send("Hello Word"));
