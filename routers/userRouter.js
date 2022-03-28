947ca48a1c0744d0fd9124d9ef00c77241625082
Mino Cha (Isaac) <mino.cha@emory.edu>
2022-03-28 19:05:51 -0400

    add getting quote of user

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -44,7 +44,7 @@ userRouter.post('/api/login', async (req, res) => {
                 lastName: user.lastName,
 				email: user.email,
 			},
-			'secret123'
+			'22secretEmory22'
 		)
 
 		return res.json({ status: 'ok', user: token })
@@ -52,5 +52,39 @@ userRouter.post('/api/login', async (req, res) => {
 		return res.json({ status: 'error', user: false })
 	}
 })
+
+userRouter.get('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, '22secretEmory22')
+		const email = decoded.email
+		const user = await User.findOne({ email: email })
+
+		return res.json({ status: 'ok', quote: user.quote })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
+
+userRouter.post('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, '22secretEmory22')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email },
+			{ $set: { quote: req.body.quote } }
+		)
+
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
+
 
 module.exports = userRouter;
